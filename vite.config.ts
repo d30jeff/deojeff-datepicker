df76d242641cd1d98e37efdf8f6c7d0e301cf461
deojeff <d30jeff@gmail.com>
2023-08-28 15:28:55 +0800

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
+import path from 'path';
 import { defineConfig } from 'vite';
-import react from 'vite-preset-react';
+import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      injectReact: true,
-      removeDevtoolsInProd: true,
-    }),
-    tsconfigPaths(),
-  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'deojeff-datepicker',
+      fileName: (format) => {
+        return `index.${format}.js`;
+      },
+    },
+    rollupOptions: {
+      external: ['reactl', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), tsconfigPaths(), dts()],
 });
